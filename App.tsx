35186acdc272d45d38b294c167f755d364109fd9
otaviobaroni <otaviobaroni223@gmail.com>
2024-10-07 21:55:32 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { ImageBackground } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+// import Stripes from './src/assets/stripes.svg'
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
+}
